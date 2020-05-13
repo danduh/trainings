405dc92155a16260a21347f2bcd82b2d456e2ded
Daniel Ostrovsky <daniel.ostrovsky@kaltura.com>
2020-05-13 12:42:50 +0300

    calculations

---
+++ js/templates.js
@@ -0,0 +1,34 @@
+const templates = {};
+templates.mainMenu = document.createElement('template');
+templates.mainMenu.innerHTML = `
+            <nav class="menu">
+                <ul>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/about.html">About</a></li>
+                    <li><a href="/contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        `;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// templates.footer = document.createElement('template');
+// templates.footer.innerHTML = `
+//             <nav class="menu">
+//                 <div><a href="/index.html">Home</a></div>
+//                 <div><a href="/about.html">About</a></div>
+//                 <div><a href="/contact.html">Contact</a></div>
+//             </nav>
+//         `;
